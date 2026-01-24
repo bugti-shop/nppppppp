423631ea18f84cf203c4e03e894b3762e39f2386
bugti-shop <127424091+bugti-shop@users.noreply.github.com>
2026-01-24 05:05:20 +0500

    Update appId and server URL in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,11 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.c4920824037c4205bb9ed6cc0d5a0385',
+  appId: 'nota.npd.com',
   appName: 'Npd',
   webDir: 'dist',
   server: {
-    url: 'https://c4920824-037c-4205-bb9e-d6cc0d5a0385.lovableproject.com?forceHideBadge=true',
+    
     cleartext: true
   },
   plugins: {
